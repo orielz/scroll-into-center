2841d1c4b58aff9cc95c4fa1a2e01127ad4212a1
Oriel Zaken <oriel.z@seebo.com>
2017-10-17 18:39:52 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require('smoothscroll-polyfill').polyfill();
+
+function scrollIntoCenter(elem, options) {
+
+    // Read options.
+    if (options === undefined)  options = {};
+    if (options.vertical === undefined)  options.vertical = 0.5;
+    if (options.horizontal === undefined)  options.horizontal = 0.5;
+
+    var container = options.container || elem.ownerDocument.defaultView;
+    var isContainer = options.container;
+
+    // Fetch positional information.
+    var rect = elem.getBoundingClientRect();
+    var containerRect = isContainer && container.getBoundingClientRect();
+
+
+    // Determine location to scroll to.
+    var targetY = container[isContainer ? 'scrollTop' : 'scrollY'] + (rect.top - (isContainer ? containerRect.top : 0)) - ((container[isContainer ? 'clientHeight' : 'innerHeight']) - elem.offsetHeight) * options.vertical;
+    var targetX = container[isContainer ? 'scrollLeft' : 'scrollX'] + (rect.left - (isContainer ? containerRect.left : 0)) - ((container[isContainer ? 'clientWidth' : 'innerWidth']) - elem.offsetWidth) * options.horizontal;
+
+    // Scroll.
+    container.scroll(targetX, targetY);
+
+    // If window is inside a frame, center that frame in the parent window. Recursively.
+    if (!isContainer && container.parent !== container) {
+        // We are inside a scrollable element.
+        var frame = container.frameElement;
+        scrollIntoCenter.call(frame, options);
+    }
+
+}
+
+module.exports = scrollIntoCenter;
